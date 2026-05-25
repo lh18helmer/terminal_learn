779970d7ca5cb7be70fbf07dfa65a3da0b5e7799
logan helmer <loganhelmer6@gmail.com>
2026-05-25 14:08:53 -0400

    c testing

---
+++ c_practice.c
@@ -0,0 +1,16 @@
+
+#include <stdio.h>
+
+int main() {
+
+    printf("hello world");
+    
+    char name[12] = "Logan Helmer";
+    int array[6] = {1, 4, 6, 67, 34, 78};
+
+    int array[] = "Logan Helmer";
+
+    printf("%s", name);
+
+    return 0;
+}
